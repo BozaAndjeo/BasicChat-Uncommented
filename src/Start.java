5930daa54acbf6e7ed7f17eda53c073c39930b29
Runelord <Runelord@Runelord-PC>
2023-11-05 09:17:25 +0100

    Project Basic Chat

---
+++ src/Start.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+import view.MainWindow;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Runelord
+ */
+public class Start {
+    public static void main(String[] args) {
+        MainWindow mw=new MainWindow();
+        mw.activate();
+    }
+}
